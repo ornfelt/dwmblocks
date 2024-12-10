71be29188b1b357dbc645a918ec0a3604a8a1f79
ornfelt <jonasornfelt@outlook.com>
2024-12-10 06:57:20 +0100

    mul_mo

--- Makefile
+++ Makefile
@@ -1,57 +1,19 @@
 .POSIX:
 
-BIN := dwmblocks
-BUILD_DIR := build
-SRC_DIR := src
-INC_DIR := include
+PREFIX = /usr/local
+CC = gcc
 
-DEBUG := 0
-VERBOSE := 0
-LIBS := xcb-atom
-
-PREFIX := /usr/local
-CFLAGS := -Ofast -I. -I$(INC_DIR) -std=c99
-CFLAGS += -DBINARY=\"$(BIN)\" -D_POSIX_C_SOURCE=200809L
-CFLAGS += -Wall -Wpedantic -Wextra -Wswitch-enum
-CFLAGS += $(shell pkg-config --cflags $(LIBS))
-LDLIBS := $(shell pkg-config --libs $(LIBS))
-
-SRCS := $(wildcard $(SRC_DIR)/*.c)
-OBJS := $(subst $(SRC_DIR)/,$(BUILD_DIR)/,$(SRCS:.c=.o))
-
-INSTALL_DIR := $(DESTDIR)$(PREFIX)/bin
-
-# Prettify output
-PRINTF := @printf "%-8s %s\n"
-ifeq ($(VERBOSE), 0)
-	Q := @
-endif
-
-ifeq ($(DEBUG), 1)
-	CFLAGS += -g
-endif
-
-all: $(BUILD_DIR)/$(BIN)
-
-$(BUILD_DIR)/%.o: $(SRC_DIR)/%.c config.h
-	$Qmkdir -p $(@D)
-	$(PRINTF) "CC" $@
-	$Q$(COMPILE.c) -o $@ $<
-
-$(BUILD_DIR)/$(BIN): $(OBJS)
-	$(PRINTF) "LD" $@
-	$Q$(LINK.o) $^ $(LDLIBS) -o $@
-
+dwmblocks: dwmblocks.o
+	$(CC) dwmblocks.o -lX11 -o dwmblocks
+dwmblocks.o: dwmblocks.c config.h
+	$(CC) -c dwmblocks.c
 clean:
-	$(PRINTF) "CLEAN" $(BUILD_DIR)
-	$Q$(RM) $(BUILD_DIR)/*
-
-install: $(BUILD_DIR)/$(BIN)
-	$(PRINTF) "INSTALL" $(INSTALL_DIR)/$(BIN)
-	$Qinstall -D -m 755 $< $(INSTALL_DIR)/$(BIN)
-
+	rm -f *.o *.gch dwmblocks
+install: dwmblocks
+	mkdir -p $(DESTDIR)$(PREFIX)/bin
+	cp -f dwmblocks $(DESTDIR)$(PREFIX)/bin
+	chmod 755 $(DESTDIR)$(PREFIX)/bin/dwmblocks
 uninstall:
-	$(PRINTF) "RM" $(INSTALL_DIR)/$(BIN)
-	$Q$(RM) $(INSTALL_DIR)/$(BIN)
+	rm -f $(DESTDIR)$(PREFIX)/bin/dwmblocks
 
-.PHONY: all clean install uninstall
+.PHONY: clean install uninstall
--- config.h
+++ config.h
@@ -1,32 +1,32 @@
-#ifndef CONFIG_H
-#define CONFIG_H
+//Modify this file to change what commands output to your statusbar, and recompile using the make command.
+static const Block blocks[] = {
+    /*Icon*/        /*Command*/                                     /*Update Interval*/     /*Update Signal*/
+    /* {"^c1^",        "~/.local/bin/my_scripts/spotify_dwmblocks.sh", 5,                      12}, */
+    {"",            "~/.local/bin/my_scripts/spotify_dwmblocks.sh", 5,                      12},
+    /* {"",            "~/.local/bin/statusbar/sb-internet",           5,                      6}, */
+    /* {"",            "~/.local/bin/statusbar/sb-forecast",           18000,                  20}, */
+    /* {"",            "~/.local/bin/statusbar/sb-volumetwo.sh",       0,                      11}, */
 
-// String used to delimit block outputs in the status.
-#define DELIMITER "  "
+    {"^2^   ",     "~/.local/bin/statusbar/weather",               1800,                   5},
+    /* {"   ",        "~/.local/bin/statusbar/weather",               1800,                   5}, */
+    {"^3^  ",      "~/.local/bin/statusbar/cputemp",               5,                      4},
+    /* {"  ",         "~/.local/bin/statusbar/cputemp",               5,                      4}, */
+    /* {"^4^ ",        "~/.local/bin/statusbar/sb-volume",             0,                      10}, */
+    {"^4^ ",        "~/.local/bin/statusbar/sb-volume",             0,                      10},
+    /* {" ",           "~/.local/bin/statusbar/sb-volume",             0,                      10}, */
+    {"^5^ ",        "~/.local/bin/statusbar/sb-internet",           5,                      3},
+    /* {"^5^ ",        "~/.local/bin/statusbar/sb-battery_col",        5,                      3}, */
+    /* {" ",           "~/.local/bin/statusbar/sb-battery",            5,                      3}, */
+    {"^6^  ",      "~/.local/bin/statusbar/sb-clock",              60,                     1},
+    /* {"  ",         "~/.local/bin/statusbar/sb-clock",              60,                     1}, */
+};
 
-// Maximum number of Unicode characters that a block can output.
-#define MAX_BLOCK_OUTPUT_LENGTH 45
+/*Icons:            婢 墳  */
 
-// Control whether blocks are clickable.
-#define CLICKABLE_BLOCKS 1
+//Sets delimiter between status commands. NULL character ('\0') means no delimiter.
+/* static char *delim = " | "; */
+static char *delim = " ";
 
-// Control whether a leading delimiter should be prepended to the status.
-#define LEADING_DELIMITER 0
-
-// Control whether a trailing delimiter should be appended to the status.
-#define TRAILING_DELIMITER 0
-
-// Define blocks for the status feed as X(icon, cmd, interval, signal).
-#define BLOCKS(X)             \
-    X("", "sb-mail", 600, 1)  \
-    X("", "sb-music", 0, 2)   \
-    X("", "sb-disk", 1800, 3) \
-    X("", "sb-memory", 10, 4) \
-    X("", "sb-loadavg", 5, 5) \
-    X("", "sb-mic", 0, 6)     \
-    X("", "sb-record", 0, 7)  \
-    X("", "sb-volume", 0, 8)  \
-    X("", "sb-battery", 5, 9) \
-    X("", "sb-date", 1, 10)
-
-#endif  // CONFIG_H
+// Have dwmblocks automatically recompile and run when you edit this file in
+// vim with the following line in your vimrc/init.vim:
+// autocmd BufWritePost ~/.local/src/dwmblocks/config.h !cd ~/.local/src/dwmblocks/; sudo make install && { killall -q dwmblocks;setsid dwmblocks & }
